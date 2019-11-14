aedf07aec1ec52f7468f5f52fd7a48e5a647c5a8
Colin <qingyu.wang@aliyun.com>
2019-11-14 22:08:32 +0800

    chore: remove cli and update readme and ci

--- cli/index.js
+++
@@ -1,8 +0,0 @@
-const sec = require("./src/sections");
-
-const inquirer = require("inquirer");
-
-const addon = require("./build/Release/dscliaddon.node");
-
-// inquirer.prompt(sec).catch(e => console.log(e));
-console.log(addon);
